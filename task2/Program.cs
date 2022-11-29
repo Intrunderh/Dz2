25500b9a9b573ac8e2e5f40b3d71f3de2bd4fb4d
Albert <albert.obychenie@moe.ru>
2022-11-29 19:09:26 +0500

    Сделал все, кроме 2

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,24 +1,11 @@
 ﻿Console.Write("Input three-digit nubmer: ");
 int num = Convert.ToInt32(Console.ReadLine());
-int count;
-if (count >= num % 100 % 10)
+int cifra;
+if (cifra >= num % 100 % 10)
 {
-    Console.Write($"The third digit of {num} is {count}");
+    Console.Write($"The third digit of {num} is {cifra}");
 }
-else
+else if (cifra < num % 100 % 10)
 {
     Console.WriteLine("No third digit");
 }
-
-
-// int InputSecondNumber(int num)
-// {
-//     int count;
-//     count = num % 100 / 10;
-//     return count;
-// }
-
-// Console.Write("Input three-digit nubmer: ");
-// int num = Convert.ToInt32(Console.ReadLine());
-// int secondNumber = InputSecondNumber(num);
-// Console.Write($"The second digit of {num} is {secondNumber}");
--- task3/Program.cs
+++ task3/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход цифру, 
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+
+Console.Write("Enter the day of the week: ");
+int day = Convert.ToInt32(Console.ReadLine());
+switch (day)
+{
+    case 1:
+        Console.WriteLine("No");
+        break;
+    case 2:
+        Console.WriteLine("No");
+        break;
+    case 3:
+        Console.WriteLine("No");
+        break;
+    case 4:
+        Console.WriteLine("No");
+        break;
+    case 5:
+        Console.WriteLine("No");
+        break;
+    case 6:
+        Console.WriteLine("Yes");
+        break;
+    case 7:
+        Console.WriteLine("Yes");
+        break;
+
+}
