790200e7c505b6fd94f6927b129e7e39d3827bdf
Albert <albert.obychenie@moe.ru>
2022-11-29 18:05:35 +0500

    Сделал задачу 1

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int InputSecondNumber(int num)
+{
+    int count;
+    count = num % 100 / 10;
+    return count;
+}
+
+Console.Write("Input three-digit nubmer: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int secondNumber = InputSecondNumber(num);
+Console.Write($"The second digit of {num} is {secondNumber}");
--- task2/Program.cs
+++ task2/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Input three-digit nubmer: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int count;
+if (count >= num % 100 % 10)
+{
+    Console.Write($"The third digit of {num} is {count}");
+}
+else
+{
+    Console.WriteLine("No third digit");
+}
+
+
+// int InputSecondNumber(int num)
+// {
+//     int count;
+//     count = num % 100 / 10;
+//     return count;
+// }
+
+// Console.Write("Input three-digit nubmer: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int secondNumber = InputSecondNumber(num);
+// Console.Write($"The second digit of {num} is {secondNumber}");
